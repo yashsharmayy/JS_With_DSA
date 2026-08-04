c662eafeed0b8bbc81504ae9fc3c963b25464e31
yash sharma <ys4468570@gmail.com>
2026-08-04 14:23:07 +0530

    Add examples for type casting, swapping variables, and basic arithmetic operations

--- DSA-js/basic logic/js1.js
+++ DSA-js/basic logic/js1.js
@@ -27,9 +27,50 @@ console.log("5" * 5); // 25
 console.log("5" / 5); // 1
 
 //------------
-
+//type casting or type conversion
 console.log(Number(12)); // 12 num
 console.log(Number("12")); // 12 num
 console.log(Number("sharma")); // NaN
 
 //-------------
+//swapping
+//Method 1
+let a1 = 10;
+let b1 = 20;
+let c1;
+
+c1 = a1; // copy to c=10
+a1 = b1; // a=20 , b=20
+b1 = c1; // b = 10 , a = 20
+console.log(a1, b1);
+
+//method 2
+
+let a2 = 10;
+let b2 = 20;
+
+a2 = a2 + b2; // 10 + 20 = 30
+b2 = a2 - b2; // 30-20 = 10
+a2 = a2 - b2; // 30-10 = 20
+
+console.log(a2, b2);
+
+// method 3
+let a3 = 10;
+let b3 = 20;
+
+[a3, b3] = [b3, a3];
+console.log(a3, b3);
+
+//
+let g = 12;
+let h = 5;
+
+console.log(g / h); // 2.4 -- qutioant
+console.log(g % h); // 2 -- reminder
+console.log(Math.floor(g / h)); // 2 -- integer
+
+console.log(h % g); // 5 -- because divident is smaller then divisor
+// 5<12
+
+//
