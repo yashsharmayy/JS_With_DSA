006016bd3f98cce3babf52d5b7b566555e87f0c6
yash sharma <ys4468570@gmail.com>
2026-08-18 00:16:13 +0530

    Refactor sum calculation logic in js3_Loops.js and update input validation

--- DSA-js/basic logic/js3_Loops.js
+++ DSA-js/basic logic/js3_Loops.js
@@ -8,3 +8,25 @@ for (let i = 1; i < 23; i++) {
 for (let i = 200; i > 99; i--) {
   console.log(i);
 }
+
+// for (let i = 1; i < 10; ) {
+// //   console.log(i); // infinite
+// }
+
+//-------sum of N natural numbers-------
+
+// let n = Number(prompt("Enter your no"))
+let n = 7;
+if (isNaN(n)) {
+  console.log("enter a valid no");
+} else {
+  if (n > 0) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    console.log(sum);
+  } else {
+    console.log("no should be n=0or n>0");
+  }
+}
